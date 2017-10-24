d3e30d3f24b130839dbfdca3e7ce87e14033177b
EvanderVegt <e.vandervegt03@gmail.com>
2017-10-24 19:26:21 +0200

    hij is af

--- index.php
+++ index.php
@@ -4,8 +4,19 @@
             
     echo $kleurStoplicht;
     echo $afstandTotStoplicht;
+    echo "<br><br>";
     
+    if($kleurStoplicht == "rood"){//als het rood is
+        echo "stoppen";
+         
+    }else{
+        if($kleurStoplicht == "geel" and $afstandTotStoplicht >15){// als oranje en dichtbij
+            echo "stoppen";     
+        }else{
+            echo"doorrijden";
+    }
     
+    } 
     
     
     ?>
